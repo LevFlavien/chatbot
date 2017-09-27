843b1e081394eb3c946b3363e4d165b0777062c1
Romain ETIENNE <romain.etienne@epsi.fr>
2017-09-27 11:20:18 +0200

    Bug test

--- src/main/java/app/configuration/ConfigBean.java
+++ src/main/java/app/configuration/ConfigBean.java
@@ -2,11 +2,19 @@ package app.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import com.mongodb.Mongo;
 
 import app.gestion.GestionUser;
 
 @Configuration
 public class ConfigBean {
+	
+	@Bean
+	public MongoTemplate mongoTemplate(){
+		return new MongoTemplate(new Mongo(), "chatbot");
+	}
 
 	@Bean
 	public GestionUser gestionUser() {
--- src/test/java/app/gestion/GestionUserTests.java
+++ src/test/java/app/gestion/GestionUserTests.java
@@ -6,14 +6,16 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import app.bean.User;
 import app.configuration.ConfigBean;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
-@ContextConfiguration(classes = { ConfigBean.class, GestionUserTests.class })
+@RunWith(SpringJUnit4ClassRunner.class)
+//ApplicationContext will be loaded from the OrderServiceConfig class
+@ContextConfiguration(classes=ConfigBean.class, loader=AnnotationConfigContextLoader.class)
 public class GestionUserTests {
 	
 	@Autowired
